dbccd68e3dccd036f597a941e8ac8669a555d3fd
Le Dinh Dung <dev.dungld@gmail.com>
2024-10-09 10:31:01 +0700

    feature/api-post-comments

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('post', [PostController::class, 'getPost']); // Lấy danh sách bài viết
+Route::get('post/popular', [PostController::class, 'getPopularPost']); // Lấy bài viết được xem nhiều nhất
+Route::get('post/latest', [PostController::class, 'getLatestPost']); // Lấy bài viết mới nhất
+Route::post('post/{id}/comment', [PostController::class, 'postComment']); // Bình luận trên bài viết
+Route::post('post/{id}/comment', [PostController::class, 'postComment']); // Bình luận trên bài viết
+Route::get('post/{id}/comments', [PostController::class, 'getPostComments']); // Lấy danh sách bình luận
+Route::get('post/{id}/share', [PostController::class, 'getShareableLink']); // Chia sẻ qua mạng xã hội
+Route::delete('comment/{id}', [PostController::class, 'deleteComment']); // Xóa bình luận
